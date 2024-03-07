9ebb0cdcf199d8f031a6b511a06bcb6a81664e21
Gnarkill33 <gnarkill33@gmail.com>
2024-03-07 23:44:37 +0300

    hook_useState

--- src/components/RateList.jsx
+++ src/components/RateList.jsx
@@ -3,10 +3,11 @@ import styles from './rateList.module.css';
 import Records from '../records.json';
 
 
+
 function RateList() {
      return (
         <div className={styles.wrapper}> 
-            {Records.map((record) => <Rate title={record.title} price={record.price} speed={record.speed} text={record.text} color={record.color} size={record.size}/>)}
+            {Records.map((record) => <Rate key={record.id} title={record.title} price={record.price} speed={record.speed} text={record.text} color={record.color}/>)}
         </div>
     )
 }
@@ -14,3 +15,12 @@ function RateList() {
 export default RateList
 
 
+// function RateList() {
+//     return (
+//        <div className={styles.wrapper}> 
+//            {Records.map((record) => <Rate title={record.title} price={record.price} speed={record.speed} text={record.text} color={record.color} size={record.size}/>)}
+//        </div>
+//    )
+// }
+
+// export default RateList
